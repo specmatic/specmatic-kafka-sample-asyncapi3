343ee865069e43be034f6a284bb98e0e790a3506
Yogesh Ananda Nikam <yogeshnikam671@gmail.com>
2026-02-09 15:42:32 +0530

    Revert "fix: set the EX_DIR env var within container"
    This reverts commit b5507a4af311bd5ed9ddda825f46894323b43aef.

--- src/test/kotlin/com/example/order/ContractTestUsingTestContainer.kt
+++ src/test/kotlin/com/example/order/ContractTestUsingTestContainer.kt
@@ -42,15 +42,15 @@ class ContractTestUsingTestContainer {
         @JvmStatic
         fun isNonCIOrLinux(): Boolean =
             System.getenv("CI") != "true" || System.getProperty("os.name").lowercase().contains("linux")
+    }
 
-        @JvmStatic
-        fun isLinuxCI(): Boolean {
-            return System.getenv("CI") == "true" && System.getProperty("os.name").lowercase().contains("linux")
-        }
+    private fun isLinuxCI(): Boolean {
+        return System.getenv("CI") == "true" && System.getProperty("os.name").lowercase().contains("linux")
+    }
 
-        init {
-            if(isLinuxCI()) LocalExamplesDir.setup()
-        }
+    @BeforeAll
+    fun setup() {
+        if(isLinuxCI()) LocalExamplesDir.setup()
     }
 
     @AfterAll
@@ -66,9 +66,6 @@ class ContractTestUsingTestContainer {
                 "/usr/src/app",
                 BindMode.READ_WRITE,
             )
-            .apply {
-                if (isLinuxCI()) withEnv("EX_DIR", System.getProperty("EX_DIR"))
-            }
             .waitingFor(Wait.forLogMessage(".*Failed:.*", 1))
             .withNetworkMode("host")
             .withLogConsumer { print(it.utf8String) }
@@ -82,9 +79,6 @@ class ContractTestUsingTestContainer {
                 "/usr/src/app",
                 BindMode.READ_WRITE,
             )
-            .apply {
-                if (isLinuxCI()) withEnv("EX_DIR", System.getProperty("EX_DIR"))
-            }
             .waitingFor(Wait.forHttp("/actuator/health").forStatusCode(200))
             .withNetworkMode("host")
             .withLogConsumer { print(it.utf8String) }
